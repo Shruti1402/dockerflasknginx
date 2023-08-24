4228ce02493cb8570bae99a58f1334e332851ac3
Shruti1402 <88777172+Shruti1402@users.noreply.github.com>
2023-08-24 10:17:02 +0530

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World from flask new '
+    return 'Hello World from flask new new '
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8000, debug=True)
